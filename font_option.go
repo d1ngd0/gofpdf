a3b37c12047ba21ac9d73ff4d38121596add337c
Paul Montag <paul.david.montag@gmail.com>
2019-02-01 14:13:54 -0600

    Made font style converter exported

--- font_option.go
+++ font_option.go
@@ -4,25 +4,31 @@ import (
 	"strings"
 )
 
-//Regular - font style regular
-const Regular = 0 //000000
-//Italic - font style italic
-const Italic = 1 //000001
-//Bold - font style bold
-const Bold = 2 //000010
-//Underline - font style underline
-const Underline = 4 //000100
+const (
+	//Regular - font style regular
+	Regular = 0 //000000
+	//Italic - font style italic
+	Italic = 1 //000001
+	//Bold - font style bold
+	Bold = 2 //000010
+	//Underline - font style underline
+	Underline = 4 //000100
+)
+
+func GetConvertedStyle(fontStyle string) int {
+	return getConvertedStyle(fontStyle)
+}
 
 func getConvertedStyle(fontStyle string) (style int) {
-	fontStyle  = strings.ToUpper(fontStyle)
+	fontStyle = strings.ToUpper(fontStyle)
 	if strings.Contains(fontStyle, "B") {
-		style=style|Bold
+		style = style | Bold
 	}
 	if strings.Contains(fontStyle, "I") {
-		style=style|Italic
+		style = style | Italic
 	}
-	if strings.Contains(fontStyle, "U"){
-		style=style|Underline
+	if strings.Contains(fontStyle, "U") {
+		style = style | Underline
 	}
 	return
 }
